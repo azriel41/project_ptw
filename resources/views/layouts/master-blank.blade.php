09a4033d5d3c18d205e69632e8983fa42d7d3862
RDP77 <ravidwiputra@gmail.com>
2020-03-28 19:25:02 +0700

    Changed

--- resources/views/layouts/master-blank.blade.php
+++ resources/views/layouts/master-blank.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>ITOR - IT Training Online Registration</title>
+    <title>Project Laravel</title>
     <meta content="Admin Dashboard" name="description" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <link rel="shortcut icon" href="assets/images/gg.png">
