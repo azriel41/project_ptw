e04a5d4bf15488e3ded55b36082faa5828090f42
verdyan <56811140+verdyan@users.noreply.github.com>
2020-04-02 14:24:21 +0700

    tyuyu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -23,11 +23,12 @@
                 </li>
                 <li class="menu-title">Human Resource</li>
                 <li>
-                    <a href="javascript:void(0);" class="waves-effect"><i class="ti-pie-chart"></i><span> Lebokno Dewe
-                            Coy <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span>
+                    <a href="javascript:void(0);" class="waves-effect"><i class="ti-pie-chart"></i><span> Absensi
+                        <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span>
                         </span></a>
                     <ul class="submenu">
-                        <li><a href="charts-morris">Lebokno Dewe Coy</a></li>
+                        <li><a href="charts-morris">Jadwal Karyawan</a></li>
+                        <li><a href="charts-morris">Sisa Cuti</a></li>
                     </ul>
                 </li>
                 <li class="menu-title">Produksi</li>
