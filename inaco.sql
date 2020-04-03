abaa61c1ca6336ce45802e14dfa0e4e2a63fd2a5
RDP77 <ravidwiputra@gmail.com>
2020-04-03 10:07:54 +0700

    Kalau Commit Yang Jelas Ya SU, BIAR PAHAM

--- inaco.sql
+++ inaco.sql
@@ -4,18 +4,15 @@
 -- Server OS:                    Win64
 -- HeidiSQL Version:             10.3.0.5771
 -- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 -- Dumping database structure for inaco
-CREATE DATABASE IF NOT EXISTS `inaco` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `inaco`
+/*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `inaco`;
-
 -- Dumping structure for table inaco.departemen
 CREATE TABLE IF NOT EXISTS `departemen` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -23,12 +20,10 @@ CREATE TABLE IF NOT EXISTS `departemen` (
   `nama_departemen` varchar(255) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 -- Dumping data for table inaco.departemen: ~0 rows (approximately)
 /*!40000 ALTER TABLE `departemen` DISABLE KEYS */;
 /*!40000 ALTER TABLE `departemen` ENABLE KEYS */;
-
 -- Dumping structure for table inaco.failed_jobs
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
   `id` bigint(20) unsigned NOT NULL,
@@ -38,62 +33,90 @@ CREATE TABLE IF NOT EXISTS `failed_jobs` (
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Dumping data for table inaco.failed_jobs: ~0 rows (approximately)
 /*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
 /*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
-
 -- Dumping structure for table inaco.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
   `id` int(10) unsigned NOT NULL,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Dumping data for table inaco.migrations: ~3 rows (approximately)
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-REPLACE INTO `migrations` (`id`, `migration`, `batch`) VALUES
-	(1, '2014_10_12_000000_create_users_table', 1),
-	(2, '2014_10_12_100000_create_password_resets_table', 1),
-	(3, '2019_08_19_000000_create_failed_jobs_table', 1);
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
-
+REPLACE INTO `migrations` (`id`, `migration`, `batch`)
+VALUES
+  (1, '2014_10_12_000000_create_users_table', 1),
+  (
+    2,
+    '2014_10_12_100000_create_password_resets_table',
+    1
+  ),
+  (
+    3,
+    '2019_08_19_000000_create_failed_jobs_table',
+    1
+  );
+  /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 -- Dumping structure for table inaco.password_resets
-CREATE TABLE IF NOT EXISTS `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  KEY `password_resets_email_index` (`email`(191))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+  CREATE TABLE IF NOT EXISTS `password_resets` (
+    `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    KEY `password_resets_email_index` (`email`(191))
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Dumping data for table inaco.password_resets: ~0 rows (approximately)
-/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
-
+  /*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+  /*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
 -- Dumping structure for table inaco.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) unsigned NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `fullname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nik` int(10) DEFAULT NULL,
-  `hakakses` int(10) DEFAULT NULL,
-  `last_session_id` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+  CREATE TABLE IF NOT EXISTS `users` (
+    `id` bigint(20) unsigned NOT NULL,
+    `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email_verified_at` timestamp NULL DEFAULT NULL,
+    `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `fullname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `nik` int(10) DEFAULT NULL,
+    `hakakses` int(10) DEFAULT NULL,
+    `last_session_id` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Dumping data for table inaco.users: ~0 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-REPLACE INTO `users` (`id`, `username`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `fullname`, `nik`, `hakakses`, `last_session_id`) VALUES
-	(1, 'admin', 'admin@gmail.com', NULL, '$2y$10$PLXSl/iGTjiw6Z0lAbYllueOZ03CCvRmvGL54RS9wNw75WbhqdF8C', NULL, '2020-02-13 06:51:15', '2020-02-13 06:51:15', 'administrator', 12345, 1, '');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+  /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+REPLACE INTO `users` (
+    `id`,
+    `username`,
+    `email`,
+    `email_verified_at`,
+    `password`,
+    `remember_token`,
+    `created_at`,
+    `updated_at`,
+    `fullname`,
+    `nik`,
+    `hakakses`,
+    `last_session_id`
+  )
+VALUES
+  (
+    1,
+    'admin',
+    'admin@gmail.com',
+    NULL,
+    '$2y$10$PLXSl/iGTjiw6Z0lAbYllueOZ03CCvRmvGL54RS9wNw75WbhqdF8C',
+    NULL,
+    '2020-02-13 06:51:15',
+    '2020-02-13 06:51:15',
+    'administrator',
+    12345,
+    1,
+    ''
+  );
+  /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+  /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+  /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+  /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
