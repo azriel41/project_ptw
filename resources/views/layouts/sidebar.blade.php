e0bef3375c30c64b7cb15b982a47c671f930fa85
RDP77 <ravidwiputra@gmail.com>
2020-04-06 23:40:39 +0700

    Fix Sidebar Lagi Gan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -55,8 +55,8 @@
                     <a href="javascript:void(0);" class="waves-effect"><i class="ti-receipt"></i><span> Form<span
                                 class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
                     <ul class="submenu">
-                        <li><a href="{{ route('buy') }}">Pembelian</a></li>
-                        <li><a href="charts-morris">Faktur</a></li>
+                        <li><a href="{{ route('buy') }}">Pembelian (SPP)</a></li>
+                        <li><a href="charts-morris">Faktur (Jasa)</a></li>
                     </ul>
                 </li>
                 <li class="menu-title">Penjualan</li>
